74d8b8d426b74e66c0e7c2354bde029e97efe427
robinmartis <robin.martis@gmail.com>
2025-07-27 07:32:21 +0530

    Delete src directory

--- src/test/java/com/snyder/test/object/AOSFlipkartMenuItems.java
+++
@@ -1,9 +0,0 @@
-package com.snyder.test.object;
-
-import org.openqa.selenium.By;
-
-public interface AOSFlipkartMenuItems {
-	
-	By MyAccount = By.xpath("//android.widget.TextView[@text='My Account']");
-
-}
--- src/test/java/com/snyder/test/object/IOSFlipkartHome.java
+++
@@ -1,9 +0,0 @@
-package com.snyder.test.object;
-
-import org.openqa.selenium.By;
-
-public interface IOSFlipkartHome {
-	By searchTextBox = By.id("");
-	By addToCart = By.id("");
-	By menuIcon = By.xpath("");
-}
